ae683b73ee89dd6c0d7cb14960aa5742b5970097
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-11-17 22:01:11 -0800

    formatted spacing in preamble

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #! /usr/local/epd/bin/python
 __author__ = "Shantanu H. Joshi"
 __copyright__ = "Copyright 2013, Shantanu H. Joshi Ahmanson-Lovelace Brain Mapping Center, \
-				   University of California Los Angeles"
+                 University of California Los Angeles"
 __email__ = "s.joshi@ucla.edu"
 
 
